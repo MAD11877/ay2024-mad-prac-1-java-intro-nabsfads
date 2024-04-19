fbe2f14df3f1580b6953f8d8bf043700c8930a64
nabsfads <151129287+nabsfads@users.noreply.github.com>
2024-04-19 15:33:05 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,32 +1,44 @@
 import java.util.Scanner;
 
-public class Question5
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
-  }
+public class Question5 {
+    public static void main(String[] args) {
+       
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+
+        // Array to store the integers
+        int[] numbers = new int[count];
+
+        // Reading the integers
+        for (int i = 0; i < count; i++) {
+            numbers[i] = in.nextInt();
+        }
+
+        // Variables to store the mode
+        int mode = 0;
+        int maxCount = 0;
+
+        // Loop to find the mode
+        for (int i = 0; i < count; i++) {
+            int currentNumber = numbers[i];
+            int currentCount = 0;
+
+            // Count occurrences of the current number
+            for (int j = 0; j < count; j++) {
+                if (numbers[j] == currentNumber) {
+                    currentCount++;
+                }
+            }
+
+            // Update mode if necessary
+            if (currentCount > maxCount) {
+                mode = currentNumber;
+                maxCount = currentCount;
+            }
+        }
+
+        // Print the mode
+        System.out.println(mode);
+    }
 }
