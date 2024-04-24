651ff60cd48035618d08c2960e8a7e53a9622a09
nabsfads <151129287+nabsfads@users.noreply.github.com>
2024-04-24 20:18:39 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -6,14 +6,12 @@ public class Question4 {
         Scanner in = new Scanner(System.in);
         int size = in.nextInt();
 
-        // Loop to control rows
         for (int i = size; i > 0; i--) {
-            // Loop to control columns
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
-            // Move to the next line after printing each row
             System.out.println();
         }
     }
 }
+//print
